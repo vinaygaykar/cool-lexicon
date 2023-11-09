b576482c065fc26ddd362cab1ce643845c0d4c77
Vinay Gaykar <vinay.gaykar+github@proton.me>
2023-11-09 19:21:17 +0000

    refactor: Output consumer slightly

--- cmd/cool-lexicon.go
+++ cmd/cool-lexicon.go
@@ -18,6 +18,7 @@ type ProgramArgs struct {
 	configFile               string // Location of the config file
 	shouldPerformSetupChecks bool   // true if setup checks should be performed
 	isFileBasedInput         bool   // true if the input should be read from the given file instead of the command line
+	outputFolderPath         string   // true if the output should be printed to file instead of the command line
 
 	opLookup             string // value of the LOOKUP operation, if `isFileBasedInput` is true then this is file location else this is a word to operate on
 	opSearchStartingWith string // value of the SEARCH START WITH operation, if `isFileBasedInput` is true then this is file location else this is a word to operate on
@@ -26,13 +27,15 @@ type ProgramArgs struct {
 }
 
 var (
-	args         ProgramArgs
-	wordSupplier io.SupplyInput
+	args          ProgramArgs
+	wordSupplier  io.SupplyInput
+	outputPrinter io.ConsumeOutput
 )
 
 func init() {
 	flag.BoolVar(&args.shouldPerformSetupChecks, "check", false, "Setup all necessary configs if required. This is optional, if the all configs are already setup correctly this operation will have no effect")
-	flag.BoolVar(&args.isFileBasedInput, "if", false, "This flag indicates that input words to every operation should be taken from a file present at the given location")
+	flag.BoolVar(&args.isFileBasedInput, "if", false, "This flag indicates that input words to every operation should be taken from the file passed as value to individual operation")
+	flag.StringVar(&args.outputFolderPath, "of", "", "This flag indicates that output to every operation should be printed to files (created for every operation) at given path")
 	flag.StringVar(&args.configFile, "cfg", "cool-lexicon-cfg.json", "Config file location")
 	flag.StringVar(&args.opLookup, "ex", "", "Check if the given word exist")
 	flag.StringVar(&args.opSearchStartingWith, "ss", "", "Search the lexicon to find words that start with given substring")
@@ -52,6 +55,12 @@ func main() {
 		wordSupplier = &io.SupplyWordsFromCLI{}
 	}
 
+	if len(args.outputFolderPath) == 0 {
+		outputPrinter = &io.ConsumeOutputToLog{}
+	} else {
+		outputPrinter = &io.ConsumeOutputToFile{ OutputFolderPath: args.outputFolderPath }
+	}
+
 	lxc := configs.GetLexicon(args.configFile, args.shouldPerformSetupChecks)
 	defer lxc.Close()
 
@@ -68,6 +77,7 @@ func sanitizeInputs() {
 	args.opSearchStartingWith = strings.TrimSpace(args.opSearchStartingWith)
 	args.opSearchEndingWith = strings.TrimSpace(args.opSearchEndingWith)
 	args.opAdd = strings.TrimSpace(args.opAdd)
+	args.outputFolderPath = strings.TrimSpace(args.outputFolderPath)
 }
 
 func validateInputs() {
@@ -101,7 +111,7 @@ func tryOperateExists(lxc lexicon.Lexicon) {
 		log.Printf("could not perform 'exists' for input (%s), error: %s\n", args.opLookup, err.Error())
 	} else {
 		log.Println("exists result: ")
-		(&io.ConsumeOutputToLog[bool]{}).Consume(response, false)
+		outputPrinter.ConsumeBool("lookup", response, false)
 	}
 }
 
--- configs/io/output_consumer.go
+++ configs/io/output_consumer.go
@@ -3,7 +3,6 @@ package io
 import (
 	"encoding/json"
 	"fmt"
-	"io/fs"
 	"log"
 	"os"
 
@@ -11,26 +10,37 @@ import (
 )
 
 // A ConsumeOutput provides ability to consume output of any operation supported by the lexicon.
-// It is a generic type interface to match with pkg/lexicon/OperationResult.
-type ConsumeOutput[V bool | []string] interface {
-	// Consume will consume the given map of string and OperationResult.
+// It provides a method for every type of output supported by lx.OperationResult
+type ConsumeOutput interface {
+	// ConsumeBool will consume the given map of string and OperationResult[bool].
 	// If consumeErrors is set then error message will also be consumed.
-	// Check implementations for concrete details.
-	Consume(output *map[string]lx.OperationResult[V], consumeErrors bool)
+	ConsumeBool(operation string, output *map[string]lx.OperationResult[bool], consumeErrors bool)
+
+	// Consume will consume the given map of string and OperationResult[[]string].
+	// If consumeErrors is set then error message will also be consumed.
+	ConsumeStringArray(operation string, output *map[string]lx.OperationResult[[]string], consumeErrors bool)
 }
 
 // A ConsumeOutputToLog is one of the implementation of ConsumeOutput which forwards the output
 // to the log. If logging is not configured, which is generally the case, then output is print
 // to the screen.
-type ConsumeOutputToLog[V bool | []string] struct{}
+type ConsumeOutputToLog struct{}
 
-func (co *ConsumeOutputToLog[V]) Consume(output *map[string]lx.OperationResult[V], consumeErrors bool) {
+func (co *ConsumeOutputToLog) ConsumeBool(operation string, output *map[string]lx.OperationResult[bool], consumeErrors bool) {
+	commonConsumerToLog(output, consumeErrors)
+}
+
+func (co *ConsumeOutputToLog) ConsumeStringArray(operation string, output *map[string]lx.OperationResult[[]string], consumeErrors bool) {
+	commonConsumerToLog(output, consumeErrors)
+}
+
+func commonConsumerToLog[T bool | []string](output *map[string]lx.OperationResult[T], consumeErrors bool) {
 	for k, v := range *output {
 		if v.Err != nil && consumeErrors {
 			log.Printf("%s : error : %s\n", k, v.Err.Error())
 		} else {
 			if json, err := json.Marshal(v.Value); err != nil && consumeErrors {
-				log.Printf("%s : error printing result : %s\n", k, err.Error())
+				log.Printf("%s : error : printing result : %s\n", k, err.Error())
 			} else {
 				log.Printf("%s : %v\n", k, string(json))
 			}
@@ -40,11 +50,19 @@ func (co *ConsumeOutputToLog[V]) Consume(output *map[string]lx.OperationResult[V
 
 // A ConsumeOutputToFile is one of the implementation of ConsumeOutput which forwards the output
 // to the provided file.
-type ConsumeOutputToFile[V bool | []string] struct {
-	filePath string
+type ConsumeOutputToFile struct {
+	OutputFolderPath string
 }
 
-func (co *ConsumeOutputToFile[V]) Consume(output *map[string]lx.OperationResult[V], consumeErrors bool) {
+func (co *ConsumeOutputToFile) ConsumeBool(operation string, output *map[string]lx.OperationResult[bool], consumeErrors bool) {
+	commonConsumerToFile(operation, co.OutputFolderPath, output, consumeErrors)
+}
+
+func (co *ConsumeOutputToFile) ConsumeStringArray(operation string, output *map[string]lx.OperationResult[[]string], consumeErrors bool) {
+	commonConsumerToFile(operation, co.OutputFolderPath, output, consumeErrors)
+}
+
+func commonConsumerToFile[T bool | []string](operation, filePath string, output *map[string]lx.OperationResult[T], consumeErrors bool) {
 	data := make([]byte, 0)
 
 	for k, v := range *output {
@@ -52,12 +70,12 @@ func (co *ConsumeOutputToFile[V]) Consume(output *map[string]lx.OperationResult[
 			data = append(data, fmt.Sprintf("%s : error : %s\n", k, v.Err.Error())...)
 		} else {
 			if json, err := json.Marshal(v.Value); err != nil && consumeErrors {
-				data = append(data, fmt.Sprintf("%s : error printing result : %s\n", k, err.Error())...)
+				data = append(data, fmt.Sprintf("%s : error : printing result : %s\n", k, err.Error())...)
 			} else {
 				data = append(data, fmt.Sprintf("%s : %v\n", k, string(json))...)
 			}
 		}
 	}
 
-	os.WriteFile(co.filePath, data, fs.FileMode(os.O_WRONLY))
+	os.WriteFile(filePath+"/"+operation+".txt", data, 0644)
 }
